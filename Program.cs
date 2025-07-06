cbb1843c07e9f2ba51454c12242b25370c2077a9
Tulen4ick <zaharsazanov2015@gmail.com>
2025-07-06 14:13:06 +0300

    added a check for the presence of a config file; if it is not present, logs and databases are created locally in the project folder

--- Program.cs
+++ Program.cs
@@ -5,26 +5,28 @@ public class Program
 {
     static void Main()
     {
-        var config = new ConfigurationBuilder()
-           .SetBasePath(Directory.GetCurrentDirectory())
-           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-           .AddEnvironmentVariables()
-           .Build();
         string logPath = "";
         string dbPath = "";
         try
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
             logPath = config["Logging:LogPath"] ?? throw new Exception("Configuration log key not found");
             dbPath = config["DB:DBPath"] ?? throw new Exception("Configuration db key not found");
-            if (string.IsNullOrWhiteSpace(logPath))
-                logPath = "./Logs/logs.txt";
-            if (string.IsNullOrWhiteSpace(dbPath))
-                dbPath = "./Data/db.json";
+            
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Configuration error: {ex.Message}");
         }
+        if (string.IsNullOrWhiteSpace(logPath))
+            logPath = "./Logs/logs.txt";
+        if (string.IsNullOrWhiteSpace(dbPath))
+            dbPath = "./Data/db.json";
         ConsoleUI.RunApp(logPath, dbPath);
     }
 }
